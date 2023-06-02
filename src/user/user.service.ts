9ccdc5351b208cf4f20c1777de461a8cd3fc2ee8
FravonDev <flvsadr@gmail.com>
2023-06-02 13:21:24 -0300

    refactor:  new migration

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -24,6 +24,6 @@ export class UserService {
   }
 
   findByEmail(email: string) {
-    return this.prisma.user.findUnique({ where: { email } });
+    return this.prisma.user.findFirst({ where: { email } });
   }
 }
