01858fdc28235830b1a4e5b548993a8bacd46aa0
FravonDev <flvsadr@gmail.com>
2023-06-08 16:22:11 -0300

    feat: chat auth

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -6,36 +6,80 @@ import {
   OnGatewayDisconnect,
   OnGatewayInit,
   WebSocketServer,
+  WsException,
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
 import { Server, Socket } from 'socket.io';
 import { Chat } from './entities/chat.entity';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { TokenExpiredError, JsonWebTokenError } from 'jsonwebtoken';
+import { UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './exceptions/all-exceptions.filter';
+import { WebsocketExceptionsFilter } from './exceptions/ws-exception.filter';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class ChatGateway
+@UseFilters(WebsocketExceptionsFilter)
+@UsePipes(new ValidationPipe({ transform: true }))export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
   @WebSocketServer() server: Server;
 
-  constructor(private readonly chatService: ChatService) {}
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+
   afterInit(server: Server) {
     console.log('Chat gateway initialized');
     setInterval(() => console.log(this.chatService.chats), 5000);
   }
 
-  handleConnection(client: any, ...args: any[]) {
-    // (Handshake) Aqui está em processo para ser concluida, posso criar logica para validar aqui
-    const { name, accountId } = client.handshake.query;
-    console.log('name:', name, ' Socket.io id:', client.id, "accountId", accountId);
-    const data = this.chatService.chats;
-    this.server.emit('getPreviousMessages', data);
+  async handleConnection(client: Socket, ...args: any[]) {
+    try {
+      const { access_token } = client.handshake.query as { access_token: string };
+
+      const decodedToken = this.jwtService.verify(access_token, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      const user = await this.userService.findByEmail(decodedToken.email);
+      if (!user) {
+        throw new Error('');
+      }
+      const currentUser = { ...user, password: undefined };
+      
+      client.data = {
+        currentUser,
+      };
+
+      const data = this.chatService.chats;
+      this.server.emit('getPreviousMessages', data);
+    } catch (error) {
+      if (error instanceof JsonWebTokenError){
+        throw new Error('Invalid access token')
+      }else if (error instanceof TokenExpiredError){
+        throw new Error('Access token expired');
+      }
+      client.disconnect();
+    }
   }
+
+
+
+
+
+
+
+
+
 
   handleDisconnect(client: Socket) {
     console.log(`Disconnected: ${client.id}`);
@@ -52,11 +96,16 @@ export class ChatGateway
   }
 
   @SubscribeMessage('getPreviousMessages')
-  async handleGetPreviousMessages(client: Socket, payload: { sender: string, recipient: string }): Promise<void> {
+  async handleGetPreviousMessages(
+    client: Socket,
+    payload: { sender: string; recipient: string },
+  ): Promise<void> {
     const { sender, recipient } = payload;
 
-
-    const previousMessages =  await this.chatService.getMessages(sender, recipient);
+    const previousMessages = await this.chatService.getMessages(
+      sender,
+      recipient,
+    );
     // FIXME: Selecionar mensagens anteriores do banco de dados
     // const previousMessages = await this.chatService.find({
     //   where: [
--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  providers: [ChatGateway, ChatService]
+  providers: [ChatGateway, ChatService, JwtService],
+  imports: [UserModule]
 })
 export class ChatModule {}
