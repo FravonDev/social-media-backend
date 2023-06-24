46cfe6a9f78dbf0db9baf0856992396393141553
FravonDev <flvsadr@gmail.com>
2023-06-24 11:21:59 -0300

    feat: ws typing function

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -15,6 +15,7 @@ import { CreateMessageDto } from './dto/create-message.dto';
 import { v4 as uuid } from 'uuid';
 import { UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
 import { WsCatchAllFilter } from './filters/ws-catch-all.filters';
+import { TypingDto } from './dto/typing-message.dto';
 
 @WebSocketGateway({
   cors: {
@@ -22,39 +23,40 @@ import { WsCatchAllFilter } from './filters/ws-catch-all.filters';
   },
 })
 @UseFilters(new WsCatchAllFilter())
-@UsePipes(new ValidationPipe())
+@UsePipes(new ValidationPipe({
+  transform: true,
+  whitelist: true,
+  forbidNonWhitelisted: true
+}))
 export class ChatGateway
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
-{
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
   constructor(
     private readonly chatService: ChatService,
     private readonly jwtService: JwtService,
     private readonly userService: UserService,
-  ) {}
+  ) { }
 
   afterInit(server: Server) {
     const middle = WSAuthMiddleware(this.jwtService, this.userService)
     server.use(middle)
-    console.log('Chat gateway initialized');
   }
 
   async handleConnection(client: AuthSocket, ...args: any[]) {
     this.chatService.saveUserSocketId(client.user.id, client.id);
-    
+
     const data = await this.chatService.getUserMessages(client.user.id);
-    
+
     this.server.to(client.id).emit('getPreviousMessages', data);
   }
-  
+
   handleDisconnect(client: AuthSocket) {
-    console.log(`Disconnected: ${client.id}`);
     this.chatService.removeUserSocketId(client.user.id);
   }
 
   @SubscribeMessage('sendMessage')
-  async handleSendMessage( client: AuthSocket, payload: CreateMessageDto): Promise<void> {
+  async handleSendMessage(client: AuthSocket, payload: CreateMessageDto): Promise<void> {
     const { recipientId, text } = payload;
 
     await this.chatService.createMessage({
@@ -64,7 +66,7 @@ export class ChatGateway
       text,
       sentAt: new Date()
     });
-    
+
     const recipientSocketId = this.chatService.getUserSocketId(recipientId);
 
     if (recipientSocketId) {
@@ -72,6 +74,16 @@ export class ChatGateway
         ...payload,
         sender: client.user.id
       });
+    }
+  }
+  @SubscribeMessage('typing')
+  async typing(client: AuthSocket, payload: TypingDto) {
+    const { recipientId } = payload
+
+    const recipientSocketId = this.chatService.getUserSocketId(recipientId);
+
+    if (recipientSocketId) {
+      this.server.to(recipientSocketId).emit('isTyping', true);
     }
   }
 }
--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { JwtService } from '@nestjs/jwt';
-import { UserService } from 'src/user/user.service';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { UserModule } from 'src/user/user.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
