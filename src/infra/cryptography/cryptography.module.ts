838b884f2d75189d9577e054ab88f31b503d57da
FravonDev <flvsadr@gmail.com>
2023-09-12 18:19:22 -0300

    feat: providing JwtService

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -5,12 +5,14 @@ import { BcryptHasher } from './bcrypt-hasher';
 import { Encrypter } from '@/app/cryptography/encrypter';
 import { HashComparer } from '@/app/cryptography/hash-comparer';
 import { HashGenerator } from '@/app/cryptography/hash-generator';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   providers: [
     { provide: Encrypter, useClass: JwtEncrypter },
     { provide: HashComparer, useClass: BcryptHasher },
     { provide: HashGenerator, useClass: BcryptHasher },
+    JwtService,
   ],
   exports: [Encrypter, HashComparer, HashGenerator],
 })
