e9a8cfda1093f3af7e207e2b0583d3cf5c4e058f
FravonDev <flvsadr@gmail.com>
2023-09-12 19:27:44 -0300

    feat: Cryptography changes and tests

--- src/infra/cryptography/bcrypt-hasher.ts
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -1,16 +1,15 @@
-import { hash, compare } from 'bcryptjs'
-
-import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
-import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+import { HashComparer } from '@/app/cryptography/hash-comparer';
+import { HashGenerator } from '@/app/cryptography/hash-generator';
+import { compare, hash } from 'bcrypt';
 
 export class BcryptHasher implements HashGenerator, HashComparer {
-  private HASH_SALT_LENGTH = 8
+  private HASH_SALT_LENGTH = 8;
 
   hash(plain: string): Promise<string> {
-    return hash(plain, this.HASH_SALT_LENGTH)
+    return hash(plain, this.HASH_SALT_LENGTH);
   }
 
   compare(plain: string, hash: string): Promise<boolean> {
-    return compare(plain, hash)
+    return compare(plain, hash);
   }
 }
---
+++ test/cryptography/fake-encrypter.ts
@@ -0,0 +1,7 @@
+import { Encrypter } from '@/app/cryptography/encrypter';
+
+export class FakeEncrypter implements Encrypter {
+  async encrypt(payload: Record<string, unknown>): Promise<string> {
+    return JSON.stringify(payload);
+  }
+}
