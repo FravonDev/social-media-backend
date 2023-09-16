821604a10e493f8ac6981652d52e94b14547dc45
FravonDev <flvsadr@gmail.com>
2023-09-16 08:43:41 -0300

    test: (e2e) unfollow controller End-to-end test

---
+++ src/infra/http/controllers/unfollow.controler.e2e-spec.ts
@@ -0,0 +1,88 @@
+import { AppModule } from '@/app.module';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { INestApplication } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Test } from '@nestjs/testing';
+import { hash } from 'bcrypt';
+import request from 'supertest';
+
+describe('Unfollow user (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test('[POST] /unfollow', async () => {
+    const john = await prisma.user.create({
+      data: {
+        email: 'johndoe@example.com',
+        password: await hash('12345678', 8),
+        username: 'johndoe',
+        name: 'John D.',
+        photo: null,
+        bio: null,
+      },
+    });
+    const access_token = jwt.sign({ sub: john.id });
+
+    const sarah = await prisma.user.create({
+      data: {
+        email: 'sarahs@example.com',
+        password: await hash('12345678', 8),
+        username: 'sarahs',
+        name: 'sarah S.',
+        photo: null,
+        bio: null,
+      },
+    });
+
+    await prisma.follow.create({
+      data: {
+        followerId: john.id,
+        followedId: sarah.id,
+        createdAt: new Date(),
+      },
+    });
+
+    const responseUnfollow = await request(app.getHttpServer())
+      .delete('/unfollow')
+      .set('Authorization', `Bearer ${access_token}`)
+      .send({ username: 'sarahs' });
+
+    expect(responseUnfollow.statusCode).toBe(200);
+  });
+
+  test('[POST] /unfollow - not Following', async () => {
+    const john = await prisma.user.findFirst({
+      where: {
+        username: 'johndoe',
+      },
+    });
+
+    if (!john) {
+      fail('Current User not found.');
+    }
+
+    const access_token = jwt.sign({ sub: john.id });
+
+    const responseFollow = await request(app.getHttpServer())
+      .delete('/unfollow')
+      .set('Authorization', `Bearer ${access_token}`)
+      .send({ username: 'sarahs' });
+    expect(responseFollow.statusCode).toBe(404);
+    expect(responseFollow.body.message).toEqual('Not following this user.');
+  });
+});
