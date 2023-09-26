300e5dc37eeafca5b594cb31ef806d517c6d82be
FravonDev <flvsadr@gmail.com>
2023-09-26 08:42:14 -0300

    feat: confirm account usecase, base repository

--- src/app/repositories/users-repository.ts
+++ src/app/repositories/users-repository.ts
@@ -4,5 +4,7 @@ export abstract class UsersRepository {
   abstract findByEmail(email: string): Promise<User | null>;
   abstract findByUsername(username: string): Promise<User | null>;
   abstract findById(id: string): Promise<User | null>;
+  abstract findByToken(token: string): Promise<User | null>;
   abstract create(user: User): Promise<void>;
+  abstract save(user: User): Promise<void>;
 }
