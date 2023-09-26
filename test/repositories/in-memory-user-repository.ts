b05e27d4fbeeff723ca13411e07de20d49a43db0
FravonDev <flvsadr@gmail.com>
2023-09-26 08:43:03 -0300

    test: confirm account unit test,in memory repository

--- test/repositories/in-memory-user-repository.ts
+++ test/repositories/in-memory-user-repository.ts
@@ -4,6 +4,19 @@ import { UsersRepository } from '@/app/repositories/users-repository';
 export class InMemoryUsersRepository implements UsersRepository {
   public users: User[] = [];
 
+  async findByToken(token: string): Promise<User | null> {
+    const user = this.users.find((item) => item.token === token);
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
+
+  async save(user: User): Promise<void> {
+    const index = this.users.findIndex((item) => item.id === user.id);
+    this.users[index] = user;
+  }
+
   async findById(id: string): Promise<User | null> {
     const user = this.users.find((item) => item.id.toString() === id);
     if (!user) {
