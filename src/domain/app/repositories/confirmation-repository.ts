63fdd91fa7d6d96b30de7cf4e2bb1e3bf7f9461e
FravonDev <flvsadr@gmail.com>
2024-01-22 08:52:07 -0300

    feat: confirmation entity and usecase repository

---
+++ src/domain/app/repositories/confirmation-repository.ts
@@ -0,0 +1,6 @@
+import { Confirmation } from '../entities/confirmation';
+
+export abstract class ConfirmationRepository {
+  abstract create(confirmation: Confirmation): Promise<void>;
+  abstract save(confirmation: Confirmation): Promise<void>;
+}
